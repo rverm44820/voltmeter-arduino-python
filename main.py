84bd3733346a08993bf0d4486de8b0bb10b4f91d
rverm44820 <rverm44820@gmail.com>
2022-01-30 21:52:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,59 @@
 from tkinter import *
-#from tkinter.ttk import *
-from time import *
-def update():
-    time_string = strftime("%I:%M:%S %p")
-    time_label.config(text=time_string)
+import serial
+from tkinter import messagebox
 
-    time_label.after(1000,update)
+SERIAL_PORT ='COM3'
+SERIAL_RATE = 9600
+global ttt
+global R1
+global R2
+global vout
+global vin
 
 
-window = Tk()
+window =Tk()
+window.title('DVM')
+window.geometry('270x270')
 
-time_label = Label(window,font=('Arial',50),fg='green',bg='black')
-time_label.pack()
 
-update()
+def quit():
+    window.quit()
+
+def read_volt():
+    R1 = 10000
+    R2 = 1000
+
+    try:
+        ser = serial.Serial(SERIAL_PORT,SERIAL_RATE)
+        print('connected to port')
+        reading = ser.readline()
+        print('reading  ',float(reading))
+        decoded_bytes= str(reading[0:len(reading)-2].decode('utf-8'))
+        vout = (float(decoded_bytes))
+        print('vout  ',vout)
+        #vin = (vout / (R2(R1+R2)))
+        #print('vin ',vin)
+        new_volt = ('{0:0.2f}'.format(vout))
+
+        lbl_volt.config(text=new_volt)#new_volt
+        lbl_volt.after(600,read_volt)
+
+    except:
+        print('not found')
+        #messagebox.showerror('not found','''\ cannot find  usb  to ttl hit ok to exit''')
+        #sys.exit()
+
+lbl_header =Label(window,text='Digital Voltmeter',font=('Helvetica',26))
+lbl_header.place(x=130,y=30,anchor=CENTER)
+
+lbl_volt = Label(window,text='0.00',font=('DSEG7 Classic',48))
+lbl_volt.place(x=120,y=120,anchor='center')
+
+btn_quit= Button(window,text='Quit',command=quit)
+btn_quit.place(relx=0.5,y=200,anchor='center')
+
+btn_start = Button(window,text='start',command=read_volt())
+#btn_start.place(relx=0.5,y=230,anchor='center')
+
 window.mainloop()
+
